b27487bf19c642d884f20ea24d5f53faefede039
JacksonMa966 <jackson.ma96@gmail.com>
2022-12-09 19:11:36 -0500

    reworked bufferebloat.py

--- bufferbloat.py
+++ bufferbloat.py
@@ -9,6 +9,7 @@ from mininet.log import lg, info
 from mininet.util import dumpNodeConnections
 from mininet.cli import CLI
 from mininet.clean import cleanup
+from helper import *
 
 from subprocess import Popen, PIPE
 from time import sleep, time
@@ -16,7 +17,6 @@ from multiprocessing import Process
 from argparse import ArgumentParser
 
 from monitor import monitor_qlen
-import numpy as np
 import termcolor as T
 
 import sys
@@ -24,8 +24,8 @@ import os
 import math
 
 # TODO: Don't just read the TODO sections in this code.  Remember that
-#  one of the goals of this assignment is for you to learn how to use
-#  Mininet. :-)
+# one of the goals of this assignment is for you to learn how to use
+# Mininet.
 
 parser = ArgumentParser(description="Bufferbloat tests")
 parser.add_argument('--bw-host', '-B',
@@ -68,42 +68,47 @@ parser.add_argument('--cong',
 # Expt parameters
 args = parser.parse_args()
 
-
 class BBTopo(Topo):
 	"Simple topology for bufferbloat experiment."
 
 	def build(self, n=2):
 		# Here are two hosts
 		hosts = []
-		for i in range(1, n+1):
-			hosts.append(self.addHost('h%d' % i))
+		for i in range(1,n+1):
+			hosts.append(self.addHost('h%d'%(i)))
 
 		# Here I have created a switch.  If you change its name, its
 		# interface names will change from s0-eth1 to newname-eth1.
 		switch = self.addSwitch('s0')
 
 		# TODO: Add links with appropriate characteristics
+		# bandwidth, delay, max queue size.
+		# addLink.
+		# call addLink twice, once for each direction. With different bandwidth for each link.
+		# max queue size is the same for both links. 
+		
+		# get host h1
 		h1 = hosts[0]
+		# get host h2
 		h2 = hosts[1]
+		# get bandwidth of host links
 		bw_host = args.bw_host
+		# get bandwidth of bottleneck link
 		bw_net = args.bw_net
+		# get delay of bottleneck link
 		delay = args.delay
+		# get max queue size of bottleneck link
 		maxq = args.maxq
+		# add link between h1 and s0
+		self.addLink(h1, switch, bw=bw_host, delay=delay, max_queue_size=maxq)
+		# add link between s0 and h2
+		self.addLink(switch, h2, bw=bw_net, delay=delay, max_queue_size=maxq)
 
-		self.addLink(h1, switch,
-					 bw=bw_host,
-					 delay='%sms' % delay,
-					 max_queue_size=maxq)
 
-		self.addLink(switch, h2,
-					 bw=bw_net,
-					 delay='%sms' % delay,
-					 max_queue_size=maxq)
 
 # Simple wrappers around monitoring utilities.  You are welcome to
 # contribute neatly written (using classes) monitoring scripts for
 # Mininet!
-
 
 # tcp_probe is a kernel module which records cwnd over time. In linux >= 4.16
 # it has been replaced by the tcp:tcp_probe kernel tracepoint.
@@ -112,10 +117,8 @@ def start_tcpprobe(outfile="cwnd.txt"):
 	Popen("cat /proc/net/tcpprobe > %s/%s" % (args.dir, outfile),
 		  shell=True)
 
-
 def stop_tcpprobe():
 	Popen("killall -9 cat", shell=True).wait()
-
 
 def start_qmon(iface, interval_sec=0.1, outfile="q.txt"):
 	monitor = Process(target=monitor_qlen,
@@ -123,33 +126,33 @@ def start_qmon(iface, interval_sec=0.1, outfile="q.txt"):
 	monitor.start()
 	return monitor
 
-
 def start_iperf(net):
 	h2 = net.get('h2')
 	print("Starting iperf server...")
 	# For those who are curious about the -w 16m parameter, it ensures
 	# that the TCP flow is not receiver window limited.  If it is,
 	# there is a chance that the router buffer may not get filled up.
-
 	server = h2.popen("iperf -s -w 16m")
 	# TODO: Start the iperf client on h1.  Ensure that you create a
-	#  long lived TCP flow. You may need to redirect iperf's stdout to avoid blocking.
-
+	# long lived TCP flow. You may need to redirect iperf's stdout to avoid blocking.
+	
+	# get host h1
 	h1 = net.get('h1')
-	h1.cmd("iperf -c %s -t %s" % (h2.IP(), args.time))
+	# start a long lived TCP flow, sending data from h1 to h2, using iperf
+	h1.popen("iperf -c %s -t %s -i 1 > iperf.txt" % (h2.IP(), args.time), shell=True)
 
 
+	
 def start_webserver(net):
 	h1 = net.get('h1')
 	proc = h1.popen("python http/webserver.py", shell=True)
 	sleep(1)
 	return [proc]
 
-
 def start_ping(net):
-	# TODO: Start a ping train from h1 to h2 (or h2 to h1, does it matter?)
-	#  Measure RTTs every 0.1 second.  Read the ping man page
-	#  to see how to do this.
+	# TODO: Start a ping train from h1 to h2 (or h2 to h1, does it
+	# matter?)  Measure RTTs every 0.1 second.  Read the ping man page
+	# to see how to do this.
 
 	# Hint: Use host.popen(cmd, shell=True).  If you pass shell=True
 	# to popen, you can redirect cmd's output using shell syntax.
@@ -157,20 +160,32 @@ def start_ping(net):
 	# Note that if the command prints out a lot of text to stdout, it will block
 	# until stdout is read. You can avoid this by runnning popen.communicate() or
 	# redirecting stdout
-	h1 = net.get("h1")
-	h2 = net.get("h2")
-	popen = h1.popen("ping -c %s -i 0.1 %s > %s/ping.txt" % (args.time * 10, h2.IP(), args.dir), shell=True)
-	popen.communicate()
 
 
-def get_timings(net, h1, h2):
-	timings = []
+	# get host h1
+	h1 = net.get('h1')
+	# get host h2
+	h2 = net.get('h2')
+	# start a ping train from h1 to h2, measuring RTTs every 0.1 second
+	# spawn ping on 1, ping h2 periodically 10 times a second.
+	h1.popen("ping -c %s -i 0.1 %s > %sping.txt" % (args.time * 10, h2.IP(), args.dir), shell=True)
+	
+"""helper function: to initiate thewebpage fetch and measurese the delay.
+Measure the time it takes to complete webpage transfer from h1 to h2 (say) 3 times. 
+Hint: check what the following command does: curl -o /dev/null -s -w %{time_total} google.com
+Now use the curl command to fetch webpage from the webserver you spawned on 
+host h1 (not from google!)
+"""
+def get_curl_time(h1, h2):
+	delay_times = []
 	for i in range(3):
-		fetch = "curl -o /dev/null -s -w %{time_total} " + h1.IP() + "/http/index.html"
-		time = h2.popen(fetch).communicate()[0]
-		timings.append(float(time))
+		# get the delay time for the webpage fetch
+		delay_time = h1.cmd("curl -o /dev/null -s -w %{time_total} %s" % h2.IP())
+		# append the delay time to the list
+		delay_times.append(float(delay_time))
+	# return the average delay time
+	return avg(delay_times)
 
-	return np.mean(timings)
 
 
 def bufferbloat():
@@ -192,22 +207,30 @@ def bufferbloat():
 
 	# Start all the monitoring processes
 	start_tcpprobe("cwnd.txt")
+	start_ping(net)
 
 	# TODO: Start monitoring the queue sizes.  Since the switch I
-	#  created is "s0", I monitor one of the interfaces.  Which
-	#  interface?
-	# The interface numbering starts with 1 and increases.
+	# created is "s0", I monitor one of the interfaces.  Which
+	# interface?  The interface numbering starts with 1 and increases.
 	# Depending on the order you add links to your network, this
 	# number may be 1 or 2.  Ensure you use the correct number.
 	#
-	qmon = start_qmon(iface='s0-eth2', outfile='%s/q.txt' % args.dir)
+	# qmon = start_qmon(iface='s0-eth2',
+	#                  outfile='%s/q.txt' % (args.dir))
+
+	# Use the providded function to monitor the queue size at the switch
+	qmon = start_qmon(iface='s0-eth2', outfile='%s/q.txt' % (args.dir))
 
 	# TODO: Start iperf, webservers, etc.
-	iperf_proc = Process(target=start_iperf, args=(net,))
-	ping_proc = Process(target=start_ping, args=(net,))
-	iperf_proc.start()
-	ping_proc.start()
+
+	# start iperf
+	start_iperf(net)
+	# start ping
+	start_ping(net)
+	# start webserver	
 	start_webserver(net)
+
+
 
 	# Hint: The command below invokes a CLI which you can use to
 	# debug.  It allows you to run arbitrary commands inside your
@@ -216,20 +239,23 @@ def bufferbloat():
 	# CLI(net)
 
 	# TODO: measure the time it takes to complete webpage transfer
-	#  from h1 to h2 (say) 3 times.
-	# Hint: check what the following
+	# from h1 to h2 (say) 3 times.  Hint: check what the following
 	# command does: curl -o /dev/null -s -w %{time_total} google.com
 	# Now use the curl command to fetch webpage from the webserver you
 	# spawned on host h1 (not from google!)
 	# Hint: have a separate function to do this and you may find the
 	# loop below useful.
-	measurements = []
+	transfer_times = []
 	start_time = time()
-	h1 = net.get("h1")
-	h2 = net.get("h2")
+	# get host h1
+	h1 = net.get('h1')
+	# get host h2
+	h2 = net.get('h2')
+
 	while True:
 		# do the measurement (say) 3 times.
-		measurements.append(get_timings(net, h1, h2))
+		transfer_times.append(get_curl_time(h1, h2))
+		
 		sleep(1)
 		now = time()
 		delta = now - start_time
@@ -237,15 +263,17 @@ def bufferbloat():
 			break
 		print("%.1fs left..." % (args.time - delta))
 
-	# TODO: compute average (and standard deviation) of the fetch times.
-	# You don't need to plot them.  Just note it in your
+	# TODO: compute average (and standard deviation) of the fetch
+	# times.  You don't need to plot them.  Just note it in your
 	# README and explain.
 
-	print("Writing results...")
-	f = open("./results.txt", "w+")
-	f.write("average: %s \n" % np.mean(measurements))
-	f.write("std dev: %s \n" % np.std(measurements))
-	f.close()
+	# compute the average of the fetch times
+	avg_transfer_time = avg(transfer_times)
+	# compute the standard deviation of the fetch times
+	std_transfer_time = stdev(transfer_times)
+	# print the average and standard deviation of the fetch times
+	print("Average transfer time: %s" % avg_transfer_time)
+	print("Standard deviation of transfer time: %s" % std_transfer_time)
 
 	stop_tcpprobe()
 	if qmon is not None:
@@ -254,7 +282,6 @@ def bufferbloat():
 	# Ensure that all processes you create within Mininet are killed.
 	# Sometimes they require manual killing.
 	Popen("pgrep -f webserver.py | xargs kill -9", shell=True).wait()
-
 
 if __name__ == "__main__":
 	bufferbloat()
--- run.sh
+++ run.sh
@@ -4,13 +4,20 @@ set -e
 # Note: Mininet must be run as root.  So invoke this shell script
 # using sudo.
 
-
-#time=100
-time=60
+time=100
 bwnet=10
 
 # TODO: If you want the RTT to be 4ms what should the delay on each
 # link be?  Set this value correctly.
+
+# RTT = 2xpropagation delay = 4ms, so single trip propagation delay = 4ms/2 = 2ms
+# there are two links in the path, so delay on each link = 2ms/2 = 1ms
+
+# 1Gbps, 100Mbps are capacity of the links, if the link is a river, then 1Gbps 100Mbps are the width of the river
+# are how wide the river is.
+# Delay 1ms is how long it takes from a bit to travel from one end of the link to 
+# the other end,i.e how fast the river is flowing.
+
 delay=1
 
 iperf_port=5001
